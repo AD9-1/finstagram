109df953ec1eaa920d248d29e6689e8badd551c0
Aparna Dhara <aparnadharaa@gmail.com>
2023-10-05 00:24:31 +0000

    sign_up.rb,action.rb created index.erb modified on 4.10.2023

--- app/actions.rb
+++ app/actions.rb
@@ -5,3 +5,25 @@ get '/' do
   @finstagram_posts = FinstagramPost.order(created_at: :desc)
   erb(:index)
 end
+
+
+get '/signup' do     # if a user navigates to the path "/signup",
+  @user = User.new   # setup empty @user object
+  erb(:sign_up)       # render "app/views/signup.erb"
+end
+
+post '/signup' do
+  email      = params[:email]
+  avatar_url = params[:avatar_url]
+  username   = params[:username]
+  password   = params[:password]
+
+  # instantiate and save a User
+  @user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password })
+
+    if @user.save
+      "User #{username} saved!"
+    else
+      erb(:sign_up)
+    end
+end
